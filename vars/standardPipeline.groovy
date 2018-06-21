ca4ca16cd4a1388c4938219e1e8023b349922aed
umairedu <user@umair.k>
2018-06-21 13:43:15 +0500

    add my firt var

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -15,6 +15,7 @@ def call(body) {
 	        }
 	        stage ('Build') {
 	        	sh "echo 'building ${config.projectName} ...'"
+            sh "echo 'my first var ${config.projectName} '"
 	        }
 	        stage ('Tests') {
 		        parallel 'static': {
@@ -36,4 +37,3 @@ def call(body) {
 	    }
     }
 }
-
